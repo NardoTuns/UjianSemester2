940bcfad122bd5992b874e03b686cbf10640b7db
NardoTuns <nardotuns@gmail.com>
2025-05-26 17:00:06 +0800

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -1,7 +1,8 @@
 function kirimKeSpreadsheet(nama, kelas, benar) {
-  fetch('https://script.google.com/macros/s/AKfycbx57ZXB6FuNv2Ueh3lZRDmIQtdv-iwdWlf5wje2Wmc7OO50geusK161qzt_KS09Ad0G/exec'), { // ganti dengan link Web App Anda
+  fetch('https://script.google.com/macros/s/AKfycbx57ZXB6FuNv2Ueh3lZRDmIQtdv-iwdWlf5wje2Wmc7OO50geusK161qzt_KS09Ad0G/exec'), 
+  {
     method: 'POST',
     body: JSON.stringify({ nama, kelas, benar }),
     headers: { 'Content-Type': 'application/json' }
-  });
+  };
 }
