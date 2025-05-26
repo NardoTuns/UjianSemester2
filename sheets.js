8fdd0347e11378a6627fadf0a3894493d1fd7e05
NardoTuns <nardotuns@gmail.com>
2025-05-26 17:27:27 +0800

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -1,5 +1,5 @@
 function kirimKeSpreadsheet(nama, kelas, benar) {
-  fetch('https://script.google.com/macros/s/AKfycbwJhEvnUgK-L_VKEERyzAQL3EMiiB9PB_dL1q_g8hFjuY3fYyEfybcHgfiBf0x7lJOtmg/exec, { // ganti dengan link Web App Anda
+  fetch('https://script.google.com/macros/s/AKfycbwJhEvnUgK-L_VKEERyzAQL3EMiiB9PB_dL1q_g8hFjuY3fYyEfybcHgfiBf0x7lJOtmg/exec, { 
     method: 'POST',
     body: JSON.stringify({ nama, kelas, benar }),
     headers: { 'Content-Type': 'application/json' }
